0f5593a07d22c2e4f9c05ce35560ff2204deb336
wdm-coding <wdmcoding0620@163.com>
2026-07-06 16:55:02 +0800

    微调

--- src/Layout/FnodePortalLayout/component/PortalFooter/index.tsx
+++ src/Layout/FnodePortalLayout/component/PortalFooter/index.tsx
@@ -1,8 +1,17 @@
 const PortalFooter = () => {
   return (
     <div className="w-full h-[216px] flex-shrink-0">
-      <div className="w-full h-[50%] bg-[#333333]" />
-      <div className="w-full h-[50%] bg-[#000000]" />
+      <div
+        className="search-wrap"
+        data-search-cat="data-product"
+        search-max-length="50"
+        search-min-length="1"
+        search-result-count="20"
+        track-site-search="true"
+      >
+        <input placeholder="请输入搜索关键词" search-keyword="true" type="search" />
+        <button search-submit="true">搜索</button>
+      </div>
     </div>
   );
 } 
